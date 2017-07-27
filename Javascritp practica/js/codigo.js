633f75c6be55406b251565199ae0160a607132c0
Kevin Arias <aa-kevin@hotmail.com>
2017-07-27 10:08:50 -0600

    Workshop

--- Javascritp practica/js/codigo.js
+++ Javascritp practica/js/codigo.js
@@ -4,12 +4,18 @@ $(document).ready(function(){
         /*Captura de datos escrito en los inputs*/        
         var nom = document.getElementById("nombretxt").value;
         var apel = document.getElementById("apellidotxt").value;
+        var pho = document.getElementById("phonetxt").value;
+        var ema = document.getElementById("emailtxt").value;
         /*Guardando los datos en el LocalStorage*/
         localStorage.setItem("Nombre", nom);
         localStorage.setItem("Apellido", apel);
+        localStorage.setItem("Telefono", pho);
+        localStorage.setItem("Email", ema);
         /*Limpiando los campos o inputs*/
         document.getElementById("nombretxt").value = "";
         document.getElementById("apellidotxt").value = "";
+        document.getElementById("phonetxt").value = "";
+        document.getElementById("emailtxt").value = "";
     });   
 });
 
@@ -19,8 +25,10 @@ $(document).ready(function(){
         /*Obtener datos almacenados*/
         var nombre = localStorage.getItem("Nombre");
         var apellido = localStorage.getItem("Apellido");
+        var telefono = localStorage.getItem("Telefono");
+        var correo = localStorage.getItem("Email");
         /*Mostrar datos almacenados*/      
-        document.getElementById("mostrar").innerHTML = nombre + " " +apellido;
+        document.getElementById("mostrar").innerHTML = nombre + " " +apellido + "\n" + telefono + "\n" + correo;
         
     });   
 });
